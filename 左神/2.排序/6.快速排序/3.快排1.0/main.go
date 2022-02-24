da68152a344d6d94e518c33346edc8dad8ce7b9c
thooh <rol@thooh.com>
2022-02-24 23:11:08 +0800

    feat(左神): 快排1.0

--- 左神/2.排序/6.快速排序/3.快排1.0/main.go
+++ 左神/2.排序/6.快速排序/3.快排1.0/main.go
@@ -5,40 +5,32 @@ import "fmt"
 func main() {
 	arr := []int{3, 9, 10, 30, 20, 10, 5, 7, 8, 10}
 
-	fmt.Println(quicksort(arr, 0, 9))
+	quicksort(arr, 0, len(arr))
+	fmt.Println(arr)
 }
 
-func quicksort(arr []int, start int, end int) []int {
+func quicksort(arr []int, start int, end int) {
 
-	if start < 0 || end < 0 || end < start {
-		return []int{}
+	if start < 0 || end < 0 || end <= start {
+		return
 	}
 
-	if start == end {
-		return arr[start : end+1]
+	if start+1 == end {
+		return
 	}
 
-	flag := arr[end]
+	flag := arr[end-1]
 
 	s := start - 1
-	for i := start; i < end; i++ {
+	for i := start; i < end-1; i++ {
 		if arr[i] <= flag {
 			s++
 			arr[s], arr[i] = arr[i], arr[s]
 		}
 	}
 
-	arr[s+1], arr[end] = arr[end], arr[s+1]
+	arr[s+1], arr[end-1] = arr[end-1], arr[s+1]
 
-	fmt.Println(arr[start:end], arr[start:s], arr[s+1], prints(arr, s+2, end))
-
-	return append(append(quicksort(arr, start, s), arr[s+1]), quicksort(arr, s+2, end)...)
-
-}
-
-func prints(arr []int, start, end int) []int {
-	if end < start {
-		return []int{}
-	}
-	return arr[start:end]
+	quicksort(arr, start, s+1)
+	quicksort(arr, s+2, end)
 }
