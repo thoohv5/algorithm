13454ef9c22880a0b546d903151584f26010c10d
thooh <rol@thooh.com>
2022-02-24 23:02:35 +0800

    feat(左神): 堆排序

--- 左神/2.排序/5.堆排序/1.堆基础/main.go
+++ 左神/2.排序/5.堆排序/1.堆基础/main.go
@@ -9,29 +9,14 @@ import "fmt"
 时间复杂度：O(logN)
 */
 func main() {
-	heap := []int{6, 3, 5}
-	fmt.Println(heapInsertValue(heap, 7))
-	heap, val := heapifyValue(heap)
-	fmt.Println(heap, val)
 
-	heap1 := []int{8, 7, 6, 10}
-	heapInsert(heap1, 3)
-	fmt.Println(heap1)
+	heap := []int{8, 7, 6, 10}
+	heapInsert(heap, 3)
+	fmt.Println(heap)
 
-	heap1[len(heap1)-1], heap1[0] = heap1[0], heap1[len(heap1)-1]
-	heapify(heap1, 0, 3)
-	fmt.Println(heap1)
-}
-
-// 插入
-func heapInsertValue(heap []int, num int) []int {
-	heap = append(heap, num)
-	index := len(heap) - 1
-	for heap[(index-1)/2] < heap[index] {
-		heap[(index-1)/2], heap[index] = heap[index], heap[(index-1)/2]
-		index = (index - 1) / 2
-	}
-	return heap
+	heap[len(heap)-1], heap[0] = heap[0], heap[len(heap)-1]
+	heapify(heap, 0, len(heap)-1)
+	fmt.Println(heap)
 }
 
 // 插入
@@ -43,46 +28,13 @@ func heapInsert(heap []int, index int) {
 }
 
 // 取极值
-func heapifyValue(heap []int) ([]int, int) {
-	if len(heap) == 0 {
-		return nil, 0
-	}
-	heap[0], heap[len(heap)-1] = heap[len(heap)-1], heap[0]
-
-	index := 0
-
-	target := 0
-	for index < len(heap)-2 {
-		left := 2*index + 1
-		if left < len(heap)-1 {
-			target = left
-			if right := left + 1; right < len(heap)-1 {
-				if heap[right] > heap[left] {
-					target = right
-				}
-			}
-		}
-		if heap[target] <= heap[index] {
-			break
-		}
-		heap[target], heap[index] = heap[index], heap[target]
-		index = target
-	}
-
-	val := heap[len(heap)-1]
-	return heap[0 : len(heap)-1], val
-}
-
-// 取极值
 func heapify(heap []int, index, heapSize int) {
 	target := 0
 	left := 2*index + 1
 	for left < heapSize {
 		target = left
-		if right := left + 1; right < heapSize {
-			if heap[right] > heap[left] {
-				target = right
-			}
+		if right := left + 1; right < heapSize && heap[right] > heap[left] {
+			target = right
 		}
 		if heap[target] <= heap[index] {
 			break
--- 左神/2.排序/5.堆排序/2.推排序/main.go
+++ 左神/2.排序/5.堆排序/2.推排序/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /**
 堆排序
@@ -10,56 +12,10 @@ import "fmt"
 func main() {
 	arr := []int{3, 9, 10, 30, 20, 10, 5, 7, 8, 10}
 
-	// fmt.Println(heapSort(arr))
-	fmt.Println(heapSort2(arr))
+	fmt.Println(heapSort(arr))
 }
 
 func heapSort(arr []int) []int {
-
-	l := len(arr)
-
-	for j := 1; j < l; j++ {
-		index := j
-		for arr[(index-1)/2] < arr[index] {
-			arr[(index-1)/2], arr[index] = arr[index], arr[(index-1)/2]
-			index = (index - 1) / 2
-		}
-	}
-
-	// fmt.Println(arr)
-
-	for 0 < l {
-		arr[0], arr[l-1] = arr[l-1], arr[0]
-		l = l - 1
-
-		// fmt.Println(arr, l)
-
-		index := 0
-		target := 0
-		for index < l {
-			left := 2*index + 1
-			if left < l {
-				target = left
-				if right := left + 1; right < l {
-					if arr[right] > arr[left] {
-						target = right
-					}
-				}
-			}
-			if arr[target] <= arr[index] {
-				break
-			}
-			arr[target], arr[index] = arr[index], arr[target]
-			index = target
-		}
-		// fmt.Println(arr)
-	}
-
-	return arr
-
-}
-
-func heapSort2(arr []int) []int {
 
 	l := len(arr)
 
@@ -94,10 +50,8 @@ func heapify(heap []int, index, heapSize int) {
 	left := 2*index + 1
 	for left < heapSize {
 		target = left
-		if right := left + 1; right < heapSize {
-			if heap[right] > heap[left] {
-				target = right
-			}
+		if right := left + 1; right < heapSize && heap[right] > heap[left] {
+			target = right
 		}
 		if heap[target] <= heap[index] {
 			break
