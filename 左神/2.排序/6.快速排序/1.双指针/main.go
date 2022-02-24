0627135ace37a3072ac2b4545bc32dd4481118b6
thooh <rol@thooh.com>
2022-02-24 23:01:55 +0800

    feat(左神): 双指针

--- 左神/2.排序/6.快速排序/1.双指针/main.go
+++ 左神/2.排序/6.快速排序/1.双指针/main.go
@@ -10,39 +10,17 @@ func main() {
 
 	arr := []int{3, 9, 10, 30, 20, 10, 5, 7, 8, 10}
 
-	// fmt.Println(marksA(arr, 9))
-	fmt.Println(marksB(arr, 9))
+	fmt.Println(marks(arr, 9))
 
 }
 
-func marksA(arr []int, num int) []int {
-
-	s := 0
-	e := len(arr) - 1
-	for i := 0; i < len(arr); {
-		if num >= arr[i] {
-			s++
-			i++
-			continue
-		}
-		if s >= e {
-			break
-		}
-		arr[i], arr[e] = arr[e], arr[i]
-		e--
-	}
-	return arr
-
-}
-
-func marksB(arr []int, num int) []int {
+func marks(arr []int, num int) []int {
 	s := -1
 	for i := 0; i < len(arr); i++ {
 		if arr[i] <= num {
 			s++
 			arr[s], arr[i] = arr[i], arr[s]
 		}
-		// fmt.Println(i, arr, s)
 	}
 	return arr
 
